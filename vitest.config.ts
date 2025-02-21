5eb372e8e45d1777239be2763d8234c61004042e
Yulius Tjahjadi <yultide@yahoo.com>
2025-02-20 17:09:17 -0800

    chore: fix unit test to handle coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
+		server: {
+			deps: {
+				inline: ['vitest-console'],
+			},
+		},
+		setupFiles: ['vitest.setup.ts'],
 		coverage: {
 			exclude: [...(configDefaults.coverage.exclude || []), 'src/types'],
 			// you can include other reporters, but 'json-summary' is required, json is recommended
